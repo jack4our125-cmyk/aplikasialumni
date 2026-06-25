a7963f1072b20186b43f8f99c227c81ce34b3b50
afifurrohman <jack4our125@gmail.com>
2026-06-25 15:35:40 +0700

    satu

---
+++ src/alumni202557201024/Alumni202557201024.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package alumni202557201024;
+
+/**
+ *
+ * @author Hype G12
+ */
+public class Alumni202557201024 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/alumni202557201024/koneksi.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package alumni202557201024;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Hype G12
+ */
+public class koneksi {
+    //mendeklarasikan variabel koneksi sebagai static agar bia diakses dari mana saja di dalm class
+    private static Connection mysqlconfig;
+    //method static untuk membuka koneksi ke database mysql
+    public static Connection konek(){
+        
+        try {
+            String url = "jdbc;mysql://localhost:3306/alumni_202557201024";
+            
+            String user = "root";
+            
+            String pass = "itmdigdaya";
+            
+            mysqlconfig = DriverManager.getConnection(url, user, pass);
+            
+        } catch (SQLException sQLException) {
+            
+            System.err.println(sQLException.getMessage());
+        }
+ 
+        return mysqlconfig;
+    }
+    
+}
